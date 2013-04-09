8b2c1325868576606df969cb420210a8c13c7127
Ilia <yakov.the.smart@gmail.com>
2013-04-10 00:43:26 +0300

    Working with DB. Update core files

--- test6.php
+++ test6.php
@@ -23,7 +23,8 @@
         </div>
 
         <script>
-            Backbone.emulateHTTP = true; // Use _method parameter rather than using DELETE and PUT methods
+
+           // Backbone.emulateHTTP = true; // Use _method parameter rather than using DELETE and PUT methods
             Backbone.emulateJSON = true; // Send data to server via parameter rather than via request content
 
             var Person = Backbone.Model.extend({
@@ -46,16 +47,17 @@
                 }
             });
 
-            var person = new Person({id: 1});
-            person.fetch(); // fetch model from DB with id = 1
 
-            person = new Person({name: "Joe Zim", age: 23});
-            person.save(); // create and save a new model on the server, also get id back and set it
-
-            person = new Person({id: 1, name: "Joe Zim", age: 53});
-            person.save(); // update the model on the server (it has an id set, therefore it is on the server already)
-//            person.destroy(); // delete the model from the server
-
+//            var person = new Person({id: 1});
+//            person.fetch(); // fetch model from DB with id = 1
+//
+//            person = new Person({name: "Joe Zim", age: 23});
+//            person.save(); // create and save a new model on the server, also get id back and set it
+//
+//            person = new Person({id: 1, name: "Joe Zim", age: 53});
+//            person.save(); // update the model on the server (it has an id set, therefore it is on the server already)
+////            person.destroy(); // delete the model from the server
+//
             var People = Backbone.Collection.extend({
                 initialize: function() {
                     this.on('all', function(e) {
@@ -67,9 +69,11 @@
             });
 
             var people = new People();
+
+
             people.fetch(); // Get all models for this collection
-            people.create({name: "Joe Zim", age: 23}); // Create model, add to Collection and add to DB
-            people.create({id: 6, name: "Chuck Norris", age: 72}); // Update model: add to Collection, update DB
+//            people.create({name: "Joe Zim", age: 23}); // Create model, add to Collection and add to DB
+//            people.create({id: 6, name: "Chuck Norris", age: 72}); // Update model: add to Collection, update DB
         </script>
 
     </body>
